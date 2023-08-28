ebccfdba148d6908d315673455a87c97539c086c
Jason Silvis <JSilvis030592@yahoo.com>
2023-08-28 15:25:33 -0400

    Added navbar.css and basic styling. Added classname for navbar items

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,13 @@
 import React from "react";
+import './navbar.css';
 import { Link } from "react-router-dom";
 
 export default function Navbar(){
     return (
         <div className="navigation">
             <div className="links">
-                <Link to='/'>Shop</Link>
-                <Link to='/cart'>Cart</Link>
+                <Link className="navItem" to='/'>Shop</Link>
+                <Link className="navItem" to='/cart'>Cart</Link>
             </div>
         </div>
     )
