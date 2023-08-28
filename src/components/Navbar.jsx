b42613977836e2bd929d433fcec28470216d81ee
Jason Silvis <JSilvis030592@yahoo.com>
2023-08-28 15:09:45 -0400

    Added navigation links to Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Navbar(){
     return (
-        <div>
-            Navbar
+        <div className="navigation">
+            <div className="links">
+                <Link to='/'>Shop</Link>
+                <Link to='/cart'></Link>
+            </div>
         </div>
     )
 }
