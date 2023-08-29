be5b639bcc11297f9e6464e09546c4b66346afb2
Jason Silvis <JSilvis030592@yahoo.com>
2023-08-29 07:28:52 -0400

    Added shopping cart icon to display in navbar. Changed size of navbar and position of text to the end.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import './navbar.css';
 import { Link } from "react-router-dom";
+import {ShoppingCart} from 'phosphor-react';
 
 export default function Navbar(){
     return (
         <div className="navigation">
             <div className="links">
                 <Link  to='/'>Shop</Link>
-                <Link  to='/cart'>Cart</Link>
+                <Link  to='/cart'><ShoppingCart size={24} /></Link>
             </div>
         </div>
     )
