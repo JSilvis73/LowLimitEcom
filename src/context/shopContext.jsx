92c72dbaf5349cefba1b7fd8c6902d581c6d21e6
Jason Silvis <JSilvis030592@yahoo.com>
2023-08-30 07:32:40 -0400

    Created a shop context component to keep track of the state of our cart. Has add/remove functionality

---
+++ src/context/shopContext.jsx
@@ -0,0 +1,35 @@
+import React, {createContext, useState} from "react";
+
+export const ShopContext = createContext(null);
+
+//Looping through cart to calculate how many of the item is requested.
+const getDefaultCart = () => {
+    let cart = {};
+    for (let index = 1; index < array.length + 1; index++) {
+        cart[index] = 0;
+    }
+    return cart;
+}
+
+export const ShopContextProvider = (props) => {
+    const [cartItems, setCartItems] = useState(getDefaultCart());
+
+    // Adding to cart.
+    const addToCart = (itemId) => {
+        setCartItems((prev) => ({...prev, [itemId]: prev[itemId] + 1}));
+    }
+
+    // Removing from cart.
+    const removeFromCart = (itemId) => {
+        setCartItems((prev) => ({...prev, [itemId]: prev[itemId] - 1}));
+    }
+
+    //Creating a value to pass out of component.
+    const contextValue = {cartItems, addToCart, removeFromCart};
+
+    return (
+    <ShopContext.Provider value={contextValue}>
+        {props.children}
+        </ShopContext.Provider>
+    );
+}
