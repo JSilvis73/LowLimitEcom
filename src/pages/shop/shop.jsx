57ad5117dd4726466e6fadca8f337099920d0573
Jason Silvis <JSilvis030592@yahoo.com>
2023-09-01 07:54:42 -0400

    Created footer component and added to bottom of the shop page.

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -2,19 +2,21 @@ import React from "react";
 import './shop.css';
 import { productList } from "../../productList";
 import { Product } from "./product";
+import {Footer} from '../../components/footer';
 
 export const Shop = () => {
     return <div className="shopPage">
         <div className="shopHeader">
             <h1>Shop</h1>
         </div>
-        
-            <div className="productList">
-                {productList.map((product) => <Product 
-                data={product}
-                />)}
-            </div>
-
+        <div className="productList">
+         {productList.map((product) => <Product 
+          data={product}
+            />)}
+        </div>
+        <div className="shopFooter">
+            <Footer />
+        </div>
        
     </div>
 };
